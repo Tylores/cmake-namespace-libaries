636be0dc5598cc226576d747c546b6c018fdc0c1
psu-powerlab <44616832+psu-powerlab@users.noreply.github.com>
2021-02-12 09:42:17 -0800

    ugh still not storing files in seperate folders

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char const *argv[])
 {
     base::echo("Hello World!");
-    formal::echo("Hello there!", "John!");
+    formal::echo("Hello there, what are you doing!", "John!");
     return 0;
 }
